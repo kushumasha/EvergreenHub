e6a33a431bf497f09a483923de2c3599ec00c10f
˹ 𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 ˼ <skr346902@gmail.com>
2026-01-09 20:14:16 +0530

    Restore credit comments and update environment variables

--- info.py
+++ info.py
@@ -11,9 +11,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', '14689508'))
-API_HASH = environ.get('API_HASH', '79413cfe2d8cc93ddf1815ef588e80d5')
-BOT_TOKEN = environ.get('BOT_TOKEN', "8490343747:AAEhMVVsf0Xe9LhfoBXRjZdV0RMgeKPdf1U")
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
